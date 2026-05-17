fe33efd6bfcf801da4ae98fe5f618eb60aa3bb1d
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-05-17 16:13:02 +0300

    Add initKoin method for ios and android platforms.

--- composeApp/src/iosMain/kotlin/com/example/carog_driver/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/example/carog_driver/MainViewController.kt
@@ -2,4 +2,10 @@ package com.example.carog_driver
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initKoin()
+    }
+) {
+    App()
+}

bda95ec492a182c4d3e0e939652f7023aafb5203
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-05-18 02:05:09 +0300

    Remove register screen testing call in App function.

--- composeApp/src/commonMain/kotlin/com/example/carog_driver/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/carog_driver/App.kt
@@ -9,47 +9,48 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
 import carog_driver.composeapp.generated.resources.Res
 import carog_driver.composeapp.generated.resources.compose_multiplatform
-import com.example.carog_driver.presentation.register.RegisterScreen
 import com.example.carog_driver.presentation.theme.AppTheme
 import com.example.carog_driver.presentation.theme.CargoTheme
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 @Preview
 fun App() {
     CargoTheme{
-//        var showContent by remember { mutableStateOf(false) }
-//        Column(
-//            modifier = Modifier
-//                .background(AppTheme.colors.background)
-//                .safeContentPadding()
-//                .fillMaxSize(),
-//            horizontalAlignment = Alignment.CenterHorizontally,
-//        ) {
-//            Button(onClick = { showContent = !showContent }) {
-//                Text("Click me!")
-//            }
-//            AnimatedVisibility(showContent) {
-//                val greeting = remember { Greeting().greet() }
-//                Column(
-//                    modifier = Modifier.fillMaxWidth(),
-//                    horizontalAlignment = Alignment.CenterHorizontally,
-//                ) {
-//                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-//                    Text("Compose: $greeting", style = TextStyle(
-//                        color = AppTheme.colors.onSurface
-//                    ))
-//                }
-//            }
-//        }
-        RegisterScreen()
+        var showContent by remember { mutableStateOf(false) }
+        Column(
+            modifier = Modifier
+                .background(AppTheme.colors.background)
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting", style = TextStyle(
+                        color = AppTheme.colors.onSurface
+                    ))
+                }
+            }
+        }
     }
 }

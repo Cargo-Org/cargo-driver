7bcfd2970ec3cd0912511f3e838cc454f201f2be
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-05-30 23:49:32 +0300

    refactor: use custom reference date for iOS media filename generation

--- composeApp/src/iosMain/kotlin/com/example/carog_driver/presentation/mediapicker/MediaPickedFileExt.kt
+++ composeApp/src/iosMain/kotlin/com/example/carog_driver/presentation/mediapicker/MediaPickedFileExt.kt
@@ -3,14 +3,28 @@ package com.example.carog_driver.presentation.mediapicker
 
 import com.cargo.driver.shared.domain.model.MediaPickedFile
 import okio.ByteString.Companion.toByteString
+import platform.Foundation.NSCalendar
 import platform.Foundation.NSData
 import platform.Foundation.NSDate
-import platform.Foundation.timeIntervalSince1970
+import platform.Foundation.NSDateComponents
+import platform.Foundation.timeIntervalSinceDate
 
 internal fun NSData.toMediaPickedFile(prefix: String): MediaPickedFile {
+    val calendar = NSCalendar.currentCalendar
+    val components = NSDateComponents().apply {
+        year = 2003
+        month = 4
+        day = 27
+    }
+
+    val specificDate = calendar.dateFromComponents(components) ?: NSDate()
+
+    val secondsSinceCustomDate = NSDate().timeIntervalSinceDate(specificDate).toLong()
+
     val bytes = toByteString().toByteArray()
+
     return MediaPickedFile(
-        name = "${prefix}_${NSDate().timeIntervalSince1970.toLong()}.jpg",
+        name = "${prefix}_${secondsSinceCustomDate}.jpg",
         sizeBytes = bytes.size.toLong(),
         mimeType = "image/jpeg",
         bytes = bytes,

70109a8164e2b65453e7b559aca0a410b41edcfe
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-06-02 02:09:31 +0300

    Implement RegisterUseCase.

--- shared/src/commonMain/kotlin/com/cargo/driver/shared/domain/usecase/register/RegisterUseCase.kt
+++ shared/src/commonMain/kotlin/com/cargo/driver/shared/domain/usecase/register/RegisterUseCase.kt
@@ -4,8 +4,9 @@ import com.cargo.driver.shared.domain.model.register.RegisterRequest
 import com.cargo.driver.shared.domain.model.register.RegisterResponse
 import com.cargo.driver.shared.domain.repository.auth.AuthenticationRepository
 
+
 class RegisterUseCase(
     private val authRepo: AuthenticationRepository
 ) {
-     suspend fun invoke(registerRequest: RegisterRequest): RegisterResponse = TODO("Not yet implemented")
+     suspend fun invoke(registerRequest: RegisterRequest): RegisterResponse = authRepo.register(registerRequest)
 }

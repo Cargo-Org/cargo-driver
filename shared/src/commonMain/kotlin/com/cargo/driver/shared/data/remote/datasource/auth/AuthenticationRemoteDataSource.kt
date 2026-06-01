ac5ebf35c6813cb0befad78c77e8871cc79d036b
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-06-02 02:09:10 +0300

    implement register method in AuthenticationRemoteDataSource.

--- shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/datasource/auth/AuthenticationRemoteDataSource.kt
+++ shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/datasource/auth/AuthenticationRemoteDataSource.kt
@@ -1,5 +1,10 @@
 package com.cargo.driver.shared.data.remote.datasource.auth
 
-interface AuthenticationRemoteDataSource {
+import com.cargo.driver.shared.data.remote.dto.register.RegisterRequestDTO
+import com.cargo.driver.shared.data.remote.dto.register.RegisterResponseDTO
+import com.cargo.driver.shared.domain.result.ApiResult
 
+
+interface AuthenticationRemoteDataSource {
+    suspend fun register(registerRequest: RegisterRequestDTO): ApiResult<RegisterResponseDTO>
 }
--- shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/datasource/auth/AuthenticationRemoteDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/datasource/auth/AuthenticationRemoteDataSourceImpl.kt
@@ -2,10 +2,24 @@ package com.cargo.driver.shared.data.remote.datasource.auth
 
 import com.cargo.driver.shared.data.local.datastore.TokenStorage
 import com.cargo.driver.shared.data.remote.client.NetworkClient
+import com.cargo.driver.shared.data.remote.dto.register.RegisterRequestDTO
+import com.cargo.driver.shared.data.remote.dto.register.RegisterResponseDTO
+import com.cargo.driver.shared.data.remote.util.safeApiCall
+import com.cargo.driver.shared.domain.result.ApiResult
+import io.ktor.client.request.setBody
 
 class AuthenticationRemoteDataSourceImpl(
     private val networkClient: NetworkClient,
     private val tokenStorage: TokenStorage
 ) : AuthenticationRemoteDataSource {
 
+    override suspend fun register(registerRequest: RegisterRequestDTO): ApiResult<RegisterResponseDTO> {
+        return safeApiCall<RegisterResponseDTO> {
+            networkClient.post("register", {
+                this.setBody(
+                    registerRequest
+                )
+            })
+        }
+    }
 }

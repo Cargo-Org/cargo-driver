25262e4d664c2ea7a381e9c5cf85d4700a523c35
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-06-01 16:51:55 +0300

    handle client-side error responses in safeApiCall with custom DTOs.

---
+++ shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/dto/ClientErrorDTO.kt
@@ -0,0 +1,19 @@
+package com.cargo.driver.shared.data.remote.dto
+
+import kotlinx.serialization.Serializable
+
+sealed interface ClientErrorDTO
+
+@Serializable
+data class ValidationClientErrorDTO(
+    val status: Int,
+    val errors: Map<String, List<String>>
+): ClientErrorDTO
+
+
+@Serializable
+data class GeneralClientErrorDTO(
+    val status: Int,
+    val title: String,
+    val detail: String
+): ClientErrorDTO
--- shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/util/safeApiCall.kt
+++ shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/util/safeApiCall.kt
@@ -1,6 +1,9 @@
 package com.cargo.driver.shared.data.remote.util
 
+import com.cargo.driver.shared.data.remote.dto.GeneralClientErrorDTO
+import com.cargo.driver.shared.data.remote.dto.ValidationClientErrorDTO
 import com.cargo.driver.shared.domain.exception.CargoException
+import com.cargo.driver.shared.domain.exception.ClientErrorException
 import com.cargo.driver.shared.domain.exception.NoInternetException
 import com.cargo.driver.shared.domain.exception.NotFoundException
 import com.cargo.driver.shared.domain.exception.ServerException
@@ -9,9 +12,12 @@ import com.cargo.driver.shared.domain.exception.UnknownException
 import com.cargo.driver.shared.domain.result.ApiResult
 import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.util.network.UnresolvedAddressException
 import kotlinx.io.IOException
+import kotlinx.serialization.json.Json
+
 
 suspend inline fun <reified T> safeApiCall(
     execute: suspend () -> HttpResponse
@@ -39,6 +45,24 @@ suspend inline fun <reified T> safeApiCall(
 
             in 500..599 -> {
                 ApiResult.Error(ServerException())
+            }
+
+            in 400..499 -> {
+                val raw = response.bodyAsText()
+                val json = Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                    prettyPrint = true
+                    encodeDefaults = false
+                }
+
+                val error = try {
+                    json.decodeFromString<GeneralClientErrorDTO>(raw)
+                } catch (_: Exception) {
+                    json.decodeFromString<ValidationClientErrorDTO>(raw)
+                }
+
+                ApiResult.Error(ClientErrorException(error))
             }
 
             else -> {

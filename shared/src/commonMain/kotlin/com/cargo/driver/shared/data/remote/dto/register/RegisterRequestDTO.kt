a41ab5fd60381f7777ef0cba5d4df55c9ed4e8c4
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-06-02 02:07:50 +0300

    add @Serializable to register DTOs and clean up RegisterResponseDTO implementations.

--- shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/dto/register/RegisterRequestDTO.kt
+++ shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/dto/register/RegisterRequestDTO.kt
@@ -1,5 +1,9 @@
 package com.cargo.driver.shared.data.remote.dto.register
 
+import kotlinx.serialization.Serializable
+
+
+@Serializable
 data class RegisterRequestDTO(
     val email: String,
     val password: String,
--- shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/dto/register/RegisterResponseDTO.kt
+++ shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/dto/register/RegisterResponseDTO.kt
@@ -1,24 +1,6 @@
 package com.cargo.driver.shared.data.remote.dto.register
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed interface RegisterResponseDTO
-
-
-object SuccessRegisterResponseDTO: RegisterResponseDTO
-
-data class AuthorizationErrorRegisterResponseDTO(
-    val status: Int,
-    val title: String,
-    val detail: String
-): RegisterResponseDTO
-
-
-data class ValidationErrorRegisterResponseDTO(
-    val status: Int,
-    val errors: Map<ValidationError, List<String>>
-): RegisterResponseDTO
-
-enum class ValidationError(val errorKey: String){
-    EMAIL("Email"),
-    PHONE_NUMBER("PhoneNumber"),
-    PASSWORD("Password"),
-}

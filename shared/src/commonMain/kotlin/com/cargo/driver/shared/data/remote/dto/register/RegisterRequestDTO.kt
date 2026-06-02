1cd7bd8766a5335c47a2deaa08b9763b060edc55
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-06-03 02:56:36 +0300

    add SuccessRegisterResponseDTO

--- shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/dto/register/RegisterRequestDTO.kt
+++ shared/src/commonMain/kotlin/com/cargo/driver/shared/data/remote/dto/register/RegisterRequestDTO.kt
@@ -1,13 +1,18 @@
 package com.cargo.driver.shared.data.remote.dto.register
 
 import kotlinx.serialization.Serializable
-
+import kotlinx.serialization.SerialName
 
 @Serializable
 data class RegisterRequestDTO(
+    @SerialName("email")
     val email: String,
+    @SerialName("password")
     val password: String,
+    @SerialName("phoneNumber")
     val phoneNumber: String,
-    val fistName: String,
+    @SerialName("firstName")
+    val firstName: String,
+    @SerialName("lastName")
     val lastName: String
 )

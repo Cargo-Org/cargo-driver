cec0f624a3118dcc4d57cb4b38438a6dde05e15e
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-06-03 02:57:17 +0300

    Add FirstNameValidation and LastNameValidation to RegisterError

--- shared/src/commonMain/kotlin/com/cargo/driver/shared/domain/model/register/RegisterResponse.kt
+++ shared/src/commonMain/kotlin/com/cargo/driver/shared/domain/model/register/RegisterResponse.kt
@@ -8,6 +8,8 @@ data object RegisterSuccess : RegisterResponse
 sealed interface RegisterError : RegisterResponse {
     data object NoInternet : RegisterError
     data object EmailValidation : RegisterError
+    data object FirstNameValidation : RegisterError
+    data object LastNameValidation : RegisterError
     data object EmailAlreadyExists : RegisterError
     data object PasswordValidation : RegisterError
     data object PhoneNumberValidation : RegisterError

253929cf62685299b33ccf802411bae1eeddfe74
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-06-03 04:46:24 +0300

    Implement snackbar management in App composable using SnackbarManager and Scaffold.

--- composeApp/src/commonMain/kotlin/com/example/carog_driver/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/carog_driver/App.kt
@@ -1,17 +1,39 @@
 package com.example.carog_driver
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import com.example.carog_driver.presentation.navigation.NavGraph
 import com.example.carog_driver.presentation.navigation.Route
+import com.example.carog_driver.presentation.snackbar.CargoSnackbarHost
+import com.example.carog_driver.presentation.snackbar.SnackbarManager
 import com.example.carog_driver.presentation.theme.CargoTheme
 
 @Composable
-@Preview
 fun App() {
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    LaunchedEffect(Unit) {
+        SnackbarManager.events.collect { visuals ->
+            snackbarHostState.showSnackbar(
+                visuals = visuals
+            )
+        }
+    }
+
     CargoTheme {
-        NavGraph(
-            startDestinationRoute = Route.LoginRoute //this will change according to start screen
-        )
+        Scaffold(
+            snackbarHost = {
+                CargoSnackbarHost(
+                    snackbarHostState = snackbarHostState
+                )
+            }
+        ) { _ ->
+            NavGraph(
+                startDestinationRoute = Route.LoginRoute //this will change according to start screen
+            )
+        }
     }
 }

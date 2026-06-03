ab0dd206275e9eacd0a1905f013aabeaaa0c2f6a
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-06-03 04:46:02 +0300

    Add UiText utility for handling dynamic and resource strings

---
+++ composeApp/src/commonMain/kotlin/com/example/carog_driver/presentation/util/UiText.kt
@@ -0,0 +1,23 @@
+package com.example.carog_driver.presentation.util
+
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.getString
+
+sealed interface UiText {
+    data class DynamicString(val value: String) : UiText
+    data class Resource(val resource: StringResource) : UiText
+    data class ResourceWithArgs(val resource: StringResource,
+                                val args: List<Any>) : UiText
+}
+
+
+suspend fun UiText.asString(): String {
+    return when (this) {
+        is UiText.DynamicString -> value
+        is UiText.Resource -> getString(resource)
+        is UiText.ResourceWithArgs -> getString(
+            resource,
+            *args.toTypedArray()
+        )
+    }
+}

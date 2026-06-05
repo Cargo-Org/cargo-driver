adaa886de9437cbb6ea52ba6a57867b3947b96f7
Hendsayed25 <hendsayed853@gmail.com>
2026-06-05 18:00:07 +0300

    Add SplashViewModel to PresentationModule.

--- composeApp/src/commonMain/kotlin/com/example/carog_driver/di/PresentationModule.kt
+++ composeApp/src/commonMain/kotlin/com/example/carog_driver/di/PresentationModule.kt
@@ -1,6 +1,7 @@
 package com.example.carog_driver.di
 
 import com.example.carog_driver.presentation.screen.onboarding.viewmodel.OnboardingViewModel
+import com.example.carog_driver.presentation.screen.splash.viewmodel.SplashViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
@@ -11,4 +12,6 @@ val presentationModule = module {
             completeOnboarding = get()
         )
     }
+
+    viewModel { SplashViewModel(get()) }
 }
